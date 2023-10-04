3e3bca1f7c9677008075cc3743d336dcdf17aa0f
DimaEvchuk <74135637+DimaEvchuk@users.noreply.github.com>
2023-10-05 00:00:59 +0300

    HomePage Сделал poster

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 import "./HomePage.css";
+import QA from "./QA/QA.jsx";
+import Events from "./Events/Events.jsx";
+import Academy from "./Academy/Academy.jsx";
+import Blinks from "./Blinks/Blinks.jsx";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="home">
+      <div className="background">
+        <div className="homePage">
+          <div className="title">Сommunity</div>
+          <div className="blocks">
+            <QA />
+            <Events />
+            <Academy />
+            <Blinks />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
